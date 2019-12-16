33a3ef17b55a39906fcb3d6358997debda49d954
ilhak <renn12@naver.com>
2019-12-16 23:10:26 +0900

    db 연결 테스트

--- musimko/Controllers/HomeController.cs
+++ musimko/Controllers/HomeController.cs
@@ -4,22 +4,33 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using musimko.Models;
+using Dapper;
+using System.Data.SqlClient;
 
 namespace musimko.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private string connString;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
         {
             _logger = logger;
+            connString = configuration.GetConnectionString("DefaultConnection");
         }
 
         public IActionResult Index()
         {
+            var now = "";
+            using (var conn = new SqlConnection(connString)) 
+            {
+                now = conn.QuerySingle<string>("select getdate()");
+            }
+            ViewData["now"] = now;
             return View();
         }
 
